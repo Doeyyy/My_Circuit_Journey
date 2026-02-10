80e34dd9ac7eb831ad33ca5ad932e3c11773eaaa
Doeyyy <134491571+Doeyyy@users.noreply.github.com>
2026-02-10 16:40:43 +0100

    Set DEBUG to False for production readiness

--- Circuit_Journ/settings.py
+++ Circuit_Journ/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w-r0!)7)z@rgbgeamh%cflbs(sz-pd_tuuz#aqgfa&0!r)ne85'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
